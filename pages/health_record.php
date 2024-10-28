602b83a2cbb449a0694fd99f71e25a46f20ef8bb
TY <lrss3639@gmail.com>
2024-10-28 22:33:13 +0800

    Updated footer and created health record page

--- pages/health_record.php
+++ pages/health_record.php
@@ -4,7 +4,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Health Record</title>
+    <style>
+        table,th,td {
+            border :solid 2pt black;
+            text-align :center;
+            margin : 5px;
+            padding : 10px;
+}
+.fixedButton{
+            position: fixed;
+            bottom: 0px;
+            right: 0px; 
+            padding: 20px;
+        }
+        .roundedFixedBtn{
+          height: 60px;
+          line-height: 80px;  
+          width: 60px;  
+          font-size: 2em;
+          font-weight: bold;
+          border-radius: 50%;
+          background-color: #4CAF50;
+          color: white;
+          text-align: center;
+          cursor: pointer;
+        }
+    
+</style>
 </head>
 
 <body style="margin: 0px;">
@@ -13,9 +40,36 @@
     include_once __DIR__ . '/../server/connectDB.php';
     $conn->select_db('fitnessapp');
     include_once __DIR__ . '/../layout/header.php';
+    include_once __DIR__ . '/../layout/footer.php';
 
     renderHeader($conn);
     ?>
-</body>
 
+<table style="width:95%">
+  <tr>
+    <th>Date: </th>
+    <th>Time: </th>
+  </tr>
+  <tr>
+    <td>Weight: </td>
+    <td>Water Intake: </td>
+    <td>Exercise Type: </td>
+    <td>Duration: </td>
+    <td>Calories Burnt: </td>
+  </tr>
+  <tr>
+    <td>j</td>
+    <td>i</td>
+    <td>m</td>
+    <td>m</td>
+    <td>y</td>
+  </tr>
+</table>
+<a class="fixedButton" href="add_health_record.php" alt="Add Record">
+         <div class="roundedFixedBtn"><i class="fa fa-phone"></i></div>
+      </a>
+<?php
+renderFooter();
+?>
+</body>
 </html>

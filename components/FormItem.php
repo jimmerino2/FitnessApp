4dbc65e159d9c166257d260f150af94cf814176a
Jim <jimmerinocode@gmail.com>
2024-10-28 10:32:26 +0800

    Css Styling and SetupDB

--- components/FormItem.php
+++ components/FormItem.php
@@ -2,9 +2,9 @@
 function renderFormItemText($title = "Title", $name = "Name", $placeholder = "Placeholder")
 {
     echo "
-    <div style='padding: 5px; width: 70%'>
-        <h3>$title</h3>
-        <input type='text' name='$name' placeholder='$placeholder' style='height:20px; width: 100%;' required>
+    <div class='form-item'>
+        <h3 class='form-title'>$title</h3>
+        <input type='text' name='$name' placeholder='$placeholder' class='form-input' required>
     </div>
     ";
 }
@@ -12,11 +12,11 @@ function renderFormItemText($title = "Title", $name = "Name", $placeholder = "Pl
 function renderFormItemRadio($title = "Title", $name, $values)
 {
     echo "
-    <div style='padding: 5px; width: 70%'>
-        <h3>$title</h3>
+    <div class='form-item'>
+        <h3 class='form-title'>$title</h3>
     ";
     foreach ($values as $value => $label) {
-        echo "<input type='radio' name='$name' value='$value' style='width: auto; margin: 0px 20px 10px 0px' required checked>$label<br>";
+        echo "<input type='radio' name='$name' value='$value' class='form-radio' required checked>$label<br>";
     }
     echo "</div>";
 }
@@ -24,9 +24,9 @@ function renderFormItemRadio($title = "Title", $name, $values)
 function renderFormItemCalendar($title = "Title", $name)
 {
     echo "
-    <div style='padding: 5px; width: 70%'>
-        <h3>$title</h3>
-        <input type='date' name='$name' style='width: 100%; height: 25px' required>
+    <div class='form-item'>
+        <h3 class='form-title'>$title</h3>
+        <input type='date' name='$name' class='form-input' required>
     </div>
     ";
 }
@@ -34,9 +34,9 @@ function renderFormItemCalendar($title = "Title", $name)
 function renderFormItemEmail($title = "Title", $name = "Name", $placeholder = "Placeholder")
 {
     echo "
-    <div style='padding: 5px; width: 70%'>
-        <h3>$title</h3>
-        <input type='email' name='$name' placeholder='$placeholder' style='height:20px; width: 100%' required>
+    <div class='form-item'>
+        <h3 class='form-title'>$title</h3>
+        <input type='email' name='$name' placeholder='$placeholder' class='form-input' required>
     </div>
     ";
 }
@@ -44,9 +44,37 @@ function renderFormItemEmail($title = "Title", $name = "Name", $placeholder = "P
 function renderFormItemPassword($title = "Title", $name = "Name", $placeholder = "Placeholder")
 {
     echo "
-    <div style='padding: 5px; width: 70%'>
-        <h3>$title</h3>
-        <input type='password' name='$name' placeholder='$placeholder' style='height:20px; width: 100%' required>
+    <div class='form-item'>
+        <h3 class='form-title'>$title</h3>
+        <input type='password' name='$name' placeholder='$placeholder' class='form-input' required>
     </div>
     ";
 }
+?>
+
+<style>
+    .form-item {
+        padding: 5px;
+        width: 70%;
+        margin-bottom: 15px;
+    }
+
+    .form-title {
+        font-size: 18px;
+        margin-bottom: 8px;
+    }
+
+    .form-input {
+        height: 25px;
+        width: 100%;
+        padding: 5px;
+        font-size: 16px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+    }
+
+    .form-radio {
+        width: auto;
+        margin: 0px 20px 10px 0px;
+    }
+</style>

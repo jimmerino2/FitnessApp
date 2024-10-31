dc9f230b9d0204244353ba56cc71a834f4e38303
Liewlikseong <wwwqiling732@gmail.com>
2024-11-01 01:06:20 +0800

    Admin Page Update

--- pages/admin.php
+++ pages/admin.php
@@ -5,11 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <style>
+        td{
+            word-wrap: break-word;
+        }
+    </style>
 </head>
 
 <body style="margin: 0px;">
     <?php
-     include_once __DIR__ . '/../components/Buttons.php';
+    include_once __DIR__ . '/../components/Buttons.php';
     include_once __DIR__ . '/../server/connectDB.php';
     include_once __DIR__ . '/../server/data.php';
     include_once __DIR__ . '/../components/Tables.php';
@@ -26,21 +32,28 @@
     $consultationList = dataGetResultSql($sql, $pdo, [], ['adminConsultationID','date', 'time','nutritionistID', 'nutritionistName', 'nutritionistContact','comment', 'status', 'memberID', 'memberName','email']);
     
     $groupedConsultations = [];
-    foreach ($consultationList as $consultation) {
-    $groupedConsultations[$consultation['memberID']]['memberName'] = $consultation['memberName'];
-    $groupedConsultations[$consultation['memberID']]['consultations'][] = $consultation;
+foreach ($consultationList as $consultation) {
+    $nutritionistID = $consultation['nutritionistID'];
+    
+    // Set the nutritionist name only once
+    if (!isset($groupedConsultations[$nutritionistID]['nutritionistName'])) {
+        $groupedConsultations[$nutritionistID]['nutritionistName'] = $consultation['nutritionistName'];
     }
     
-    if (!empty($groupedConsultations)) {
-        foreach ($groupedConsultations as $memberID => $memberData) {
-            $memberName = $memberData['memberName'];
-            $memberEmail = $memberData['consultations'][0]['email']; 
-            echo "<h3 style='text-align:center; font-size:24px;'>Member: $memberName (ID: $memberID, Email: $memberEmail)</h3>";
+    // Add consultations to an array
+    $groupedConsultations[$nutritionistID]['consultations'][] = $consultation;
+}
+
+if (!empty($groupedConsultations)) {
+    foreach ($groupedConsultations as $nutritionistID => $nutritionistData) {
+        $nutritionistName = $nutritionistData['nutritionistName'];
+        echo "<h3 style='text-align:center; font-size:24px;'>Nutritionist: $nutritionistName (Nutritionist ID: $nutritionistID)</h3>";  
             
             // Create header row once
             echo "<div class='container_parent'><table>
                 <tr>
-                    <th>Nutritionist Name</th>
+                    <th>Member Name</th>
+                    <th>Member Email</th>
                     <th>Nutritionist Contact</th>
                     <th>Consultation Date</th>
                     <th>Consultation Time</th>
@@ -50,10 +63,11 @@
                 </tr>";
             
             // Add all consultations for this member
-            foreach ($memberData['consultations'] as $consultation) {
+            foreach ($nutritionistData['consultations'] as $consultation) {
                 $statusText = (!$consultation['status']) ? 'Pending Approval' : 'Approved';
                 echo "<tr>
-                    <td>{$consultation['nutritionistName']}</td>
+                    <td>{$consultation['memberName']}</td>
+                    <td>{$consultation['email']}</td>
                     <td>{$consultation['nutritionistContact']}</td>
                     <td>{$consultation['date']}</td>
                     <td>{$consultation['time']}</td>
